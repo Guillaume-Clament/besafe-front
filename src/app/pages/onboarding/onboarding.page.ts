a5753c25e9df8819f4e860faf80369322a750d20
sirine-achache <sirine.achache@etu.univ-amu.fr>
2021-04-23 15:58:53 +0200

    ajout new onboarding

---
+++ src/app/pages/onboarding/onboarding.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-onboarding',
+  templateUrl: './onboarding.page.html',
+  styleUrls: ['./onboarding.page.scss'],
+})
+export class OnboardingPage implements OnInit {
+  @ViewChild(IonSlides) slides: IonSlides;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  next() {
+    this.slides.slideNext();
+  }
+
+  skip() {
+    this.slides.slideNext();
+  }
+}
