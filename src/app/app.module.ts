0412de9988124d0046ba85b277a5f0e30de9ff52
Flore GRONDIN <grondinflore@gmail.com>
2021-05-07 10:14:10 +0200

    Implemented geolocation
    Added search-outline side btn
    Added geolocation API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Geolocation } from '@ionic-native/geolocation/ngx'
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Geolocation,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/carte/carte.page.ts
+++ src/app/pages/carte/carte.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx'
 
 declare var google: any;
 @Component({
@@ -10,15 +11,27 @@ export class CartePage {
   map:any;
 
   constructor(
+    private geo: Geolocation
   ) { 
   }
 
   ionViewDidEnter(){
-    this.map = new google.maps.Map(document.getElementById("map"),{
-      center : { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+    this.geo.getCurrentPosition().then((res) => {
+      this.map = new google.maps.Map(document.getElementById("map"),{
+        center : { lat: res.coords.latitude, lng: res.coords.longitude },
+        zoom: 8,
+      });
 
-    });
+      var marker = new google.maps.Marker({
+        position: { 
+          lat: res.coords.latitude, lng: res.coords.longitude
+        },
+        map: this.map
+      })
+    }).catch( e => {
+      console.log(e);
+    })
+    
   }
 
 }
