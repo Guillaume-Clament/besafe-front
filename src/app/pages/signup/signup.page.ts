34432c876bdac82dc19074d841a8653eb8562ad5
Flore GRONDIN <grondinflore@gmail.com>
2021-05-11 15:25:35 +0200

    Added eye icon on the pwd read

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,6 +1,8 @@
 import { Component, ContentChild, OnInit } from '@angular/core';
-import { IonInput, PickerController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertController, IonInput, PickerController } from '@ionic/angular';
 import { PickerOptions } from '@ionic/core';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
@@ -9,16 +11,28 @@ import { PickerOptions } from '@ionic/core';
 export class SignupPage implements OnInit {
   showPassword = false;
   @ContentChild(IonInput) input: IonInput;
+  passwordToggleIcon = 'eye';
 
   constructor(
-    private pickerCtrl: PickerController
+    private pickerCtrl: PickerController,
+    private router: Router,
+    private alerteCtrl: AlertController
   ) { }
 
   ngOnInit() {
   }
 
-  signUp(){
-
+  signUp(email, password){
+    
   }
 
+  togglePassword(): void{
+    this.showPassword = !this.showPassword;
+
+    if(this.passwordToggleIcon == 'eye'){
+      this.passwordToggleIcon = 'eye-off';
+    } else {
+      this.passwordToggleIcon = 'eye';
+    }
+  }
 }
